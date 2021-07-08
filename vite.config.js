19f87509286776a15a0a2fbf8a93b1c4c2da95b2
Cristen Jones <cjones3@mbta.com>
2021-07-08 14:06:29 -0400

    try the build again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: "/component-trials--carbon/"
+  base: "/component-trials--carbon/",
+  build: {
+    lib: {
+      name: "test",
+      entry: 'index.html',
+      formats: ["cjs"]
+    },
+    // rollupOptions: {
+    //   external: [
+    //     'carbon-components'
+    //   ],
+    // }
+  }
 })
