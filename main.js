ee12bcd9bf91ef2db3c44d79161cfbc65b54aaa4
Cristen Jones <cjones3@mbta.com>
2021-07-08 13:53:17 -0400

    cleanup build

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-// import './style.css'
-// import 'carbon-components/css/carbon-components.css'
-
-// Carbon Design System design tokens
-// $text-04: "" // button primary text color
-// $interactive-01: "" // button primary bg color
-// $hover-primary: "" // button primary hover bg color
-// $active-primary: "" // button primary active bg color
-// $body-short-01: "" // button font size/type/etc
-// $spacing-05: "" // button left padding (right has no variable)
-
-// import 'carbon-components/scss/components/button/_button.scss';
+// import './style.scss'
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: "/component-trials--carbon/",
-  build: {
-    lib: {
-      entry: 'index.html',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: [
-        /^lit-element/,
-        'carbon-web-components',
-        'carbon-components',
-        'carbon-components/es/globals/js/settings'
-      ]
-    }
-  }
+  base: "/component-trials--carbon/"
 })
